3c76c2fde5377af63ff5ee0a04ae3d508db9d145
Luciano Garcia-Banuelos <luciano.garcia@tec.mx>
2021-10-22 10:23:37 -0500

    Snapshot del proyecto, 19/Oct

---
+++ lib/bulb_web/live/statechart.ex
@@ -0,0 +1,19 @@
+defmodule BulbWeb.Statechart do
+  use GenServer
+
+  def start_link(ui), do: GenServer.start_link(__MODULE__, ui)
+
+  def init(ui) do
+    {:ok, %{ui_pid: ui, st: Off}}
+  end
+
+  def handle_cast(:switch, %{ui_pid: ui, st: Off} = state) do
+    GenServer.cast(ui, :switch_on)
+    {:noreply, %{state | st: On}}
+  end
+
+  def handle_cast(:switch, %{ui_pid: ui, st: On} = state) do
+    GenServer.cast(ui, :switch_off)
+    {:noreply, %{state | st: Off}}
+  end
+end
